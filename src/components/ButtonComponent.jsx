9754a81c86ce3ebb18e4acedf8671d318d9fe71c
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2023-09-30 20:26:09 +0200

    Style: Add ButtonComponent

---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+const ButtonComponent = () => {
+  return (
+    <>
+      <Button variant="contained" className="bg-green-500">Contained</Button>
+    </>
+  );
+};
+
+export default ButtonComponent;
